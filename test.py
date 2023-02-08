40f74bb5317d479e8880eb099ce6d8e4d783c3c2
furucrmNBThi <85471285+furucrmNBThi@users.noreply.github.com>
2023-02-08 15:32:05 +0700

    Update test.py

--- test.py
+++ test.py
@@ -7,8 +7,14 @@ import time
 def test(environ, start_response):
     print("app is running")
     time.sleep(5)
-    return test(environ, start_response)
+    response_body = 'Hello World'
+    status = '200 OK'
 
+    response_headers = [('Content-Type', 'text/plain'),
+                           ('Content-Length', str(len(response_body)))]
+
+    start_response(status, response_headers)
+    return [response_body]
 
 def hello_world(request):
     name = os.environ.get('NAME')
