35900520b01830eda128dfd50b0916033d274fbe
furucrmNBThi <85471285+furucrmNBThi@users.noreply.github.com>
2023-02-08 13:21:29 +0700

    Update test.py

--- test.py
+++ test.py
@@ -2,17 +2,22 @@ from wsgiref.simple_server import make_server
 from pyramid.config import Configurator
 from pyramid.response import Response
 import os
+import time
 
 def test():
     print("app is running")
     time.sleep(5)
     return test()
+
+
 def hello_world(request):
     name = os.environ.get('NAME')
     if name == None or len(name) == 0:
         name = "world"
     message = "Hello, " + name + "!\n"
+    test()
     return Response(message)
+
 
 if __name__ == '__main__':
     port = int(os.environ.get("PORT"))
@@ -21,5 +26,4 @@ if __name__ == '__main__':
         config.add_view(hello_world, route_name='hello')
         app = config.make_wsgi_app()
     server = make_server('0.0.0.0', port, app)
-    test()
     server.serve_forever()
