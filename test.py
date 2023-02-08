ccd2c7478be38d7bcb17b632abd40406ee4f9b05
furucrmNBThi <85471285+furucrmNBThi@users.noreply.github.com>
2023-02-08 17:31:51 +0700

    Update test.py

--- test.py
+++ test.py
@@ -3,6 +3,7 @@ from pyramid.config import Configurator
 from pyramid.response import Response
 import os
 import time
+import threading
 
 def test():
     print("app is running")
@@ -13,16 +14,19 @@ def hello_world(request):
     name = os.environ.get('NAME')
     if name == None or len(name) == 0:
         name = "world"
-    message = "Hello, " + name + "!\n"
+    message = "Call outbound " + "!\n"
     return Response(message)
 
+def start_server(app):
+    server = make_server('0.0.0.0', 8080, app)
+    thread = threading.Thread(target=server.serve_forever)
+    thread.start()
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT"))
     with Configurator() as config:
         config.add_route('hello', '/')
         config.add_view(hello_world, route_name='hello')
         app = config.make_wsgi_app()
-    server = make_server('0.0.0.0', port, app)
-    server.serve_forever()
+    start_server(app)
+    test()
     
